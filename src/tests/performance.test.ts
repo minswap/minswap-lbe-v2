c111ba4143032630920579d12d6e0c3546ba49d6
Ha Quang Minh <haquangminhbnvn@gmail.com>
2024-06-19 17:00:35 +0700

    optimize collect order

--- src/tests/performance.test.ts
+++ src/tests/performance.test.ts
@@ -21,7 +21,7 @@ import { calculateInitialLiquidity, toUnit } from "../utils";
 import { LP_COLATERAL, TREASURY_MIN_ADA } from "..";
 
 let MAX_COLLECT_SELLER_COUNT = 30;
-let MAX_COLLECT_ORDER_COUNT = 42;
+let MAX_COLLECT_ORDER_COUNT = 50;
 let MAX_REFUND_ORDER_COUNT = 50;
 let MAX_REDEEM_ORDER_COUNT = 53;
 
